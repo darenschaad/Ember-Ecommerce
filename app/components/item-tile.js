aaeb9a7f2ffc7b3bb08704b5cbc5eb9defce5d63
Daren Schaad and Mike Gallegos <Guest@epicodus-71.local>
2016-04-06 10:47:24 -0700

    implement and test ability to add items to cart service

--- app/components/item-tile.js
+++ app/components/item-tile.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   actions: {
     addToCart(item) {
       this.get('cart').add(item);
+      console.log("CART:", this.get('cart.items'));
     },
   }
 });
--- app/components/new-item.js
+++ app/components/new-item.js
@@ -3,13 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     save() {
-      var params {
+      var params = {
         name: this.get('name'),
         description: this.get('description'),
         price: this.get('price'),
         image: this.get('image'),
       };
       this.sendAction('save', params);
+      this.set('name', '');
+      this.set('description', '');
+      this.set('price', '');
+      this.set('image', '');
     }
   }
 });
